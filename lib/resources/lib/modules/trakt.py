46cdea97346599595e8da6f919eb79bca5fd2f85
Filipe Laíns <ffy00.gnc@gmail.com>
2017-11-18 00:07:23 +0000

    Add trakt App Key to the settings

--- lib/resources/lib/modules/trakt.py
+++ lib/resources/lib/modules/trakt.py
@@ -32,8 +32,8 @@ from resources.lib.modules import log_utils
 from resources.lib.modules import utils
 
 BASE_URL = 'http://api.trakt.tv'
-V2_API_KEY = 'acc97918ace2b0a211957d574e7cd7c7bc7a59b9c949df625077f1d5fb107082'
-CLIENT_SECRET = '0f3e0b9096477ee0d373d1d354700449bf0fa648bef33c191db5845b346f16ef'
+V2_API_KEY = control.setting('trakt.client_id')
+CLIENT_SECRET = control.setting('trakt.client_secret')
 REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
 
 def __getTrakt(url, post=None):
